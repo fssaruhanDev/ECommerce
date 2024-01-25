2c34000119acd3f373b9e46f4d7628ab329862d3
fssaruhanDev <83281184+fssaruhanDev@users.noreply.github.com>
2024-01-25 08:28:30 +0300

    Src yapısı düzeltildi.

---
+++ src/Clients/Web/ECommerce.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,67 @@
+﻿using ECommerce.Web.Models.ShoppingCart;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ECommerce.Web.Controllers;
+
+[Route("cart")]
+public class ShoppingCartController : Controller
+{
+    Uri baseAddress = new Uri("https://localhost:7025/api");
+    private readonly HttpClient _httpClient;
+
+
+    public ShoppingCartController()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = baseAddress;
+    }
+
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        string token = HttpContext.Session?.GetString("token") ?? "";
+        if (token == "")
+        {
+            HttpContext.SignOutAsync("CookieAuth");
+            return RedirectToAction("Index", "Login");
+        }
+        var userid = HttpContext.Session.GetString("id");
+        _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        HttpResponseMessage response = _httpClient.GetAsync(baseAddress + "/cart/getsoppingcart?userId=" + userid).Result;
+
+
+        GetShoppingCartViewModel Cart = new GetShoppingCartViewModel();
+
+        if (response.IsSuccessStatusCode)
+        {
+            var data = response.Content.ReadAsStringAsync().Result;
+            Cart = JsonConvert.DeserializeObject<GetShoppingCartViewModel>(data);
+        }
+        return View(Cart);
+    }
+
+
+    [HttpPost]
+    [Route("buy")]
+    public IActionResult Buy(Guid shoppingCartId)
+    {
+        string token = HttpContext.Session?.GetString("token") ?? "";
+        if (token == "")
+        {
+            HttpContext.SignOutAsync("CookieAuth");
+            return RedirectToAction("Index", "Login");
+        }
+        _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        HttpResponseMessage response = _httpClient.GetAsync(baseAddress + "/cart/buy?shoppingCartId=" + shoppingCartId).Result;
+
+
+        GetShoppingCartViewModel Cart = new GetShoppingCartViewModel();
+
+        return RedirectToAction("Index", "cart");
+
+
+    }
+}
